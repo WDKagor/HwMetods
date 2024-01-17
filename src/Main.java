0334132652d6c012759fe2cfde03d77124d9052e
briz1 <my1queen8my1king@gmail.com>
2024-01-17 21:31:24 +0700

    All tasks completed

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
-    public static void checkLeapYear(int year) {
+    public static String checkLeapYear(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println(year + " год - високосный год");
-        } else {
-            System.out.println(year + " год - невисокосный год");
-        }
+                return " високосный ";
+            } else {
+                return " невисокосный ";
+            }
     }
 
     public static String chekingOS (int i) {
@@ -23,15 +23,35 @@ public class Main {
         }
     }
 
+    public static int colculateDeliveryDays(int distance) {
+        int days = 1;
+        if (distance >= 20) {
+           days++;
+        }
+        if (distance >= 60) {
+            days++;
+        }
+        if (distance > 100) {
+            days = -1;
+        }
+        return days;
+    }
+
     public static void main(String[] args) {
         System.out.println("Задача 1");
-        int year = 2024;
-        checkLeapYear(year);
+        int year = 2021;
+        System.out.println(year + " год - " + checkLeapYear(year) + " год");
 
         System.out.println("Задача 2");
         int clientOS = 0;
         int clientDeviceYear = 2024;
         System.out.println("Установите" + chekingYearOfProduction(clientDeviceYear) + "версию приложения для " + chekingOS(clientOS) + " по ссылке");
+
+        System.out.println("Задача 3");
+        int deliveryDistance = 83;
+        if (colculateDeliveryDays(deliveryDistance) == -1) {
+            System.out.println("Доставки нет");
+        } else System.out.println("Доставка занимает дней: " + colculateDeliveryDays(deliveryDistance));
 
     }
 }
